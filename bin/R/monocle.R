89e21b69a33aa9ed88ed305acc0b33a144f20f9c
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-09-29 14:58:08 +0100

    init monocle script

---
+++ bin/R/monocle.R
@@ -0,0 +1,92 @@
+#!/usr/bin/env Rscript
+
+# Define arguments for Rscript
+library(getopt)
+spec = matrix(c(
+  'runtype', 'l', 2, "character",
+  'cores'   , 'c', 2, "integer",
+  'custom_functions', 'm', 2, "character",
+  'networkGenes', 'd', 2, "character"
+), byrow=TRUE, ncol=4)
+opt = getopt(spec)
+
+# Set run location
+if(length(commandArgs(trailingOnly = TRUE)) == 0){
+  cat('No command line arguments provided, user defaults paths are set for running interactively in Rstudio on docker\n')
+  opt$runtype = "user"
+} else {
+  if(is.null(opt$runtype)){
+    stop("--runtype must be either 'user' or 'nextflow'")
+  }
+  if(tolower(opt$runtype) != "user" & tolower(opt$runtype) != "nextflow"){
+    stop("--runtype must be either 'user' or 'nextflow'")
+  }
+  if(tolower(opt$runtype) == "nextflow"){
+    if(is.null(opt$custom_functions) | opt$custom_functions == "null"){
+      stop("--custom_functions path must be specified in process params config")
+    }
+  }
+}
+
+# Set paths and load data
+{
+  if (opt$runtype == "user"){
+    sapply(list.files('./bin/R/custom_functions/', full.names = T), source)
+    plot.path = "./output/plots/antler/"
+    rds.path = "./output/RDS.files/antler/"
+    antler.path = "./output/antler/"
+    data.path = "./output/RDS.files/seurat_STACAS/"
+    
+    ncores = 8
+    
+  } else if (opt$runtype == "nextflow"){
+    cat('pipeline running through nextflow\n')
+    
+    sapply(list.files(opt$custom_functions, full.names = T), source)
+    plot.path = "./plots/"
+    rds.path = "./RDS.files/"
+    antler.path = "./antler/"
+    data.path = "./"
+    
+    ncores = opt$cores
+  }
+  
+  cat(paste0("script ran with ", ncores, " cores\n"))
+  
+  dir.create(plot.path, recursive = T)
+  dir.create(rds.path, recursive = T)
+  dir.create(antler.path, recursive = T)
+  
+  # Load packages - packages are stored within renv in the repository
+  reticulate::use_python('/usr/bin/python3.7')
+  library(Seurat)
+  
+  library(future)
+  library(dplyr)
+  library(Antler)
+  library(cowplot)
+  library(clustree)
+  library(gridExtra)
+  library(grid)
+  library(pheatmap)
+  library(biomaRt)
+  library(RColorBrewer)
+}
+
+# read in seurat data
+seurat_out <- readRDS(paste0(data.path, "seurat_out_hh4filt.RDS"))
+
+
+BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
+                       'limma', 'S4Vectors', 'SingleCellExperiment',
+                       'SummarizedExperiment', 'batchelor', 'Matrix.utils'))
+
+install.packages()
+
+cds <- as.cell_data_set(integrated)
+cds <- cluster_cells(cds)
+p1 <- plot_cells(cds, show_trajectory_graph = FALSE)
+p2 <- plot_cells(cds, color_cells_by = "partition", show_trajectory_graph = FALSE)
+wrap_plots(p1, p2)
+
+
