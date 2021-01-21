92931c07fba828fc0fc6a06484eeff0d85b2d505
Eva <eva.hamrud@kcl.ac.uk>
2021-01-21 17:10:23 +0000

    playing with output location

--- NF-downstream_analysis/bin/test_1.R
+++ NF-downstream_analysis/bin/test_1.R
@@ -1,6 +1,6 @@
-#!/usr/bin/env Rscript
+# !/usr/bin/env Rscript
 
-# Define arguments for Rscript
+# # Define arguments for Rscript
 # library(getopt)
 # spec = matrix(c(
 #   'runtype', 'l', 2, "character",
@@ -33,9 +33,8 @@
 
 #     # load custom functions
 #     sapply(list.files('./NF-downstream_analysis/bin/custom_functions/', full.names = T), source)
-    
-output_path = "./output/NF-downstream_analysis/test"
-#     plot_path = "./output/NF-downstream_analysis/scRNA_analysis/output/plots/"
+#     output_path = "./output/NF-downstream_analysis/test/"
+#     plot_path = "./output/NF-downstream_analysis/test/plots"
     
 #     # set cores
 #     ncores = 8
@@ -45,17 +44,20 @@ output_path = "./output/NF-downstream_analysis/test"
     
 #     # load custom functions
 #     sapply(list.files(opt$custom_functions, full.names = T), source)
-#     output_path = "./output/"
-#     plot_path = "./output/plots/"
+#     output_path = "./output/test"
+#     plot_path = "./output/test/plots/"
     
 #     # set cores
 #     ncores = opt$cores
 #   }
   
-dir.create(output_path, recursive = T)
-#   dir.create(plot_path, recursive = T)
+# dir.create(output_path, recursive = T)
+# dir.create(plot_path, recursive = T)
 
+
+output_path = "./output/"
+dir.create(output_path, recursive = T)
 
 t <- c(1:10, 1:10)
 
-write.csv(t, paste0(output_path, "/test_1_output.csv"))
+write.csv(t, paste0(output_path, "test_1_output.csv"))
