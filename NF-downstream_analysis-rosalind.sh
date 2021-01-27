521c2643d26581d37aaa9cb2d4d9f7b1b19c2b93
Eva <eva.hamrud@kcl.ac.uk>
2021-01-27 11:40:51 +0000

    profiles

---
+++ NF-downstream_analysis-rosalind.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=otic-reprogamming
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=alex.thiery@crick.ac.uk
+
+
+## LOAD REQUIRED MODULES
+module load apps/singularity/3.5.3
+
+
+export NXF_VER=20.07.1
+
+nextflow run ./NF-downstream_analysis/main.nf \
+    -profile rosalind \
+    --input ./NF-downstream_analysis/local_samplesheet.csv
