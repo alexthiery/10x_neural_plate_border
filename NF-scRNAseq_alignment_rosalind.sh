60ec91b43a7bc2623289b8ea4cc5273144f95685
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-02-02 14:37:40 +0000

    init rosalind profile and run script

---
+++ NF-scRNAseq_alignment_rosalind.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=10x-NPB
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=alex.thiery@crick.ac.uk
+
+export TERM=xterm
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/20.07.1
+ml Singularity/3.4.2
+ml Graphviz
+
+export NXF_VER=20.07.1
+
+nextflow run ./NF-scRNAseq_alignment/main.nf \
+-profile rosalind \
+-resume
