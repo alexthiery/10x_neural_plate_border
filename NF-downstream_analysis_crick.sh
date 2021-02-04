145c76b5ec9b64122499c0751261d28c1ec24885
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-02-04 14:27:29 +0000

    init downstream analysis

--- NF-downstream_analysis_crick.sh
+++ NF-downstream_analysis_crick.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=otic-reprogamming
+#SBATCH --job-name=10x-NPB
 #SBATCH -t 72:00:00
 #SBATCH --mail-type=ALL,ARRAY_TASKS
 #SBATCH --mail-user=alex.thiery@crick.ac.uk
 
+export TERM=xterm
 
 ## LOAD REQUIRED MODULES
-module load apps/singularity/3.5.3
-
+ml purge
+ml Nextflow/20.07.1
+ml Singularity/3.4.2
+ml Graphviz
 
 export NXF_VER=20.07.1
 
 nextflow run ./NF-downstream_analysis/main.nf \
-    -profile rosalind_eva \
-    --input ./NF-downstream_analysis/local_samplesheet.csv
+--samplesheet ./samplesheets/NF-downstream_analysis/samplesheet.csv \
+-profile crick \
+-resume
