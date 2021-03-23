ca2337659895f37bc81e22be552f6a104a9720c9
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-03-23 12:12:21 +0000

    init poor cluster filt

--- NF-downstream_analysis/bin/seurat/6_poor_cluster_filt.R
+++ NF-downstream_analysis/bin/seurat/6_poor_cluster_filt.R
@@ -1,7 +1,25 @@
 #!/usr/bin/env Rscript
 
-# Define arguments for Rscript
+#!/usr/bin/env Rscript
+
+# Load packages
 library(getopt)
+reticulate::use_python('/usr/bin/python3.7')
+library(Seurat)
+library(sctransform)
+
+library(future)
+library(dplyr)
+library(cowplot)
+library(clustree)
+library(gridExtra)
+library(grid)
+library(pheatmap)
+library(RColorBrewer)
+library(tidyverse)
+
+# Define arguments for Rscript
+
 spec = matrix(c(
   'runtype', 'l', 2, "character",
   'cores'   , 'c', 2, "integer",
@@ -31,10 +49,10 @@ if(length(commandArgs(trailingOnly = TRUE)) == 0){
 {
   if (opt$runtype == "user"){
     sapply(list.files('./NF-downstream_analysis/bin/custom_functions/', full.names = T), source)
-    plot_path = "./output/NF-downstream_analysis/poor_cluster_filt/plots/"
-    rds_path = "./output/NF-downstream_analysis/poor_cluster_filt/rds_files/"
+    plot_path = "./output/NF-downstream_analysis/5_contamination_filt/plots/"
+    rds_path = "./output/NF-downstream_analysis/5_contamination_filt/rds_files/"
 
-    data_path = "./output/NF-downstream_analysis/contamination_filt/rds_files/"
+    data_path = "./output/NF-downstream_analysis/4_cell_cycle/rds_files/"
     
     ncores = 8
     
@@ -46,169 +64,87 @@ if(length(commandArgs(trailingOnly = TRUE)) == 0){
     rds_path = "./rds_files/"
     data_path = "./input/rds_files/"
     ncores = opt$cores
+
+    # Multi-core when running from command line
+    plan("multiprocess", workers = ncores)
+    options(future.globals.maxSize = 32* 1024^3) # 32gb
   }
   
   cat(paste0("script ran with ", ncores, " cores\n"))
   
   dir.create(plot_path, recursive = T)
   dir.create(rds_path, recursive = T)
-  
-  # Load packages - packages are stored within renv in the repository
-  reticulate::use_python('/usr/bin/python3.7')
-  library(Seurat)
-  library(sctransform)
-  
-  library(future)
-  library(dplyr)
-  library(cowplot)
-  library(clustree)
-  library(gridExtra)
-  library(grid)
-  library(pheatmap)
-  library(RColorBrewer)
-  library(tidyverse)
 }
 
-contamination_filt_data <- readRDS(paste0(data_path, 'sexfilt_data.RDS'))
+
+poor_cluster_filt_data <- readRDS(paste0(data_path, 'cell_cycle_data.RDS'))
+
+# Set RNA to default assay
+DefaultAssay(poor_cluster_filt_data) <- "RNA"
 
 
 ############################### Remove poor quality clusters ########################################
 
-norm.data.clustfilt <- rownames(contamination_filt_data@meta.data)[contamination_filt_data@meta.data$seurat_clusters ==  11 |
-                                                                  contamination_filt_data@meta.data$seurat_clusters == 15]
+poor_quality_cells <- rownames(filter(poor_cluster_filt_data@meta.data, seurat_clusters %in% c(11, 15)))
 
-norm.data.clustfilt <- subset(contamination_filt_data, cells = norm.data.clustfilt, invert = T)
+poor_cluster_filt_data <- subset(contamination_filt_data, cells = poor_quality_cells, invert = T)
 
 # Re-run findvariablefeatures and scaling
-norm.data.clustfilt <- FindVariableFeatures(norm.data.clustfilt, selection.method = "vst", nfeatures = 2000, assay = 'RNA')
+poor_cluster_filt_data <- FindVariableFeatures(poor_cluster_filt_data, selection.method = "vst", nfeatures = 2000, assay = 'RNA')
 
-# Enable parallelisation
-plan("multiprocess", workers = ncores)
-options(future.globals.maxSize = 2000 * 1024^2)
+poor_cluster_filt_data <- ScaleData(poor_cluster_filt_data, features = rownames(poor_cluster_filt_data), vars.to.regress = c("percent.mt", "sex", "S.Score", "G2M.Score"))
 
-norm.data.clustfilt <- ScaleData(norm.data.clustfilt, features = rownames(norm.data.clustfilt), vars.to.regress = c("percent.mt", "sex"))
-saveRDS(norm.data.clustfilt, paste0(rds.path, "norm.data.clustfilt.RDS"))
+saveRDS(poor_cluster_filt_data, paste0(rds.path, "poor_cluster_filt_data.RDS"))
 
-# Read in RDS data if needed
-# norm.data.clustfilt <- readRDS(paste0(rds.path, "norm.data.clustfilt.RDS"))
 
-# Change plot path
-curr.plot.path <- paste0(plot.path, "3_cluster_filt/")
-dir.create(curr.plot.path)
+# Set Integrated to default assay
+DefaultAssay(poor_cluster_filt_data) <- "integrated"
+
+# Rescale data on integrated assay
+poor_cluster_filt_data <- ScaleData(poor_cluster_filt_data, features = rownames(poor_cluster_filt_data), vars.to.regress = c("percent.mt", "sex", "S.Score", "G2M.Score"))
 
 # PCA
-norm.data.clustfilt <- RunPCA(object = norm.data.clustfilt, verbose = FALSE)
+poor_cluster_filt_data <- RunPCA(object = poor_cluster_filt_data, verbose = FALSE)
 
-png(paste0(curr.plot.path, "dimHM.png"), width=30, height=50, units = 'cm', res = 200)
-DimHeatmap(norm.data.clustfilt, dims = 1:30, balanced = TRUE, cells = 500)
+png(paste0(plot_path, "dimHM.png"), width=30, height=50, units = 'cm', res = 200)
+DimHeatmap(poor_cluster_filt_data, dims = 1:30, balanced = TRUE, cells = 500)
 graphics.off()
 
-png(paste0(curr.plot.path, "elbowplot.png"), width=24, height=20, units = 'cm', res = 200)
-print(ElbowPlot(norm.data.clustfilt, ndims = 40))
+png(paste0(plot_path, "elbowplot.png"), width=24, height=20, units = 'cm', res = 200)
+print(ElbowPlot(poor_cluster_filt_data, ndims = 40))
 graphics.off()
 
-png(paste0(curr.plot.path, "UMAP_PCA_comparison.png"), width=40, height=30, units = 'cm', res = 200)
-PCA.level.comparison(norm.data.clustfilt, PCA.levels = c(10, 20, 30, 40), cluster_res = 0.5)
+png(paste0(plot_path, "UMAP_PCA_comparison.png"), width=40, height=30, units = 'cm', res = 200)
+PCA.level.comparison(poor_cluster_filt_data, PCA.levels = c(10, 20, 30, 40), cluster_res = 0.5)
 graphics.off()
 
 # Use PCA=15 as elbow plot is relatively stable across stages
-norm.data.clustfilt <- FindNeighbors(norm.data.clustfilt, dims = 1:30, verbose = FALSE)
-norm.data.clustfilt <- RunUMAP(norm.data.clustfilt, dims = 1:30, verbose = FALSE)
+poor_cluster_filt_data <- FindNeighbors(poor_cluster_filt_data, dims = 1:30, verbose = FALSE)
+poor_cluster_filt_data <- RunUMAP(poor_cluster_filt_data, dims = 1:30, verbose = FALSE)
 
 # Find optimal cluster resolution
-png(paste0(curr.plot.path, "clustree.png"), width=70, height=35, units = 'cm', res = 200)
-clust.res(seurat.obj = norm.data.clustfilt, by = 0.2, prefix = "integrated_snn_res.")
+png(paste0(plot_path, "clustree.png"), width=70, height=35, units = 'cm', res = 200)
+clust.res(seurat.obj = poor_cluster_filt_data, by = 0.2, prefix = "integrated_snn_res.")
 graphics.off()
 
 # Use clustering resolution = 1.2
-norm.data.clustfilt <- FindClusters(norm.data.clustfilt, resolution = 1.2)
+poor_cluster_filt_data <- FindClusters(poor_cluster_filt_data, resolution = 1.2)
 
 # Plot UMAP for clusters and developmental stage
-png(paste0(curr.plot.path, "UMAP.png"), width=40, height=20, units = 'cm', res = 200)
-clust.stage.plot(norm.data.clustfilt, stage.col = "stage")
+png(paste0(plot_path, "UMAP.png"), width=40, height=20, units = 'cm', res = 200)
+clust.stage.plot(poor_cluster_filt_data, stage.col = "stage")
 graphics.off()
 
 # Find differentially expressed genes and plot heatmap of top DE genes for each cluster
-markers <- FindAllMarkers(norm.data.clustfilt, only.pos = T, logfc.threshold = 0.25)
+markers <- FindAllMarkers(poor_cluster_filt_data, only.pos = T, logfc.threshold = 0.25)
 # get automated cluster order based on percentage of cells in adjacent stages
-cluster.order = order.cell.stage.clust(seurat_object = norm.data.clustfilt, col.to.sort = seurat_clusters, sort.by = stage)
+cluster_order = order.cell.stage.clust(seurat_object = poor_cluster_filt_data, col.to.sort = seurat_clusters, sort.by = stage)
 # Re-order genes in top15 based on desired cluster order in subsequent plot - this orders them in the heatmap in the correct order
-top15 <- markers %>% group_by(cluster) %>% top_n(n = 15, wt = avg_logFC) %>% arrange(factor(cluster, levels = cluster.order))
+top15 <- markers %>% group_by(cluster) %>% top_n(n = 15, wt = avg_log2FC) %>% arrange(factor(cluster, levels = cluster_order))
 
-png(paste0(curr.plot.path, 'HM.top15.norm.data.clustfilt.png'), height = 75, width = 100, units = 'cm', res = 500)
-tenx.pheatmap(data = norm.data.clustfilt, metadata = c("seurat_clusters", "stage"), custom_order_column = "seurat_clusters",
-              custom_order = cluster.order, selected_genes = unique(top15$gene), gaps_col = "seurat_clusters", assay = 'integrated')
+png(paste0(plot_path, 'HM.top15.DE.poor_cluster_filt_data.png'), height = 75, width = 100, units = 'cm', res = 500)
+tenx.pheatmap(data = poor_cluster_filt_data, metadata = c("seurat_clusters", "stage"), custom_order_column = "seurat_clusters",
+              custom_order = cluster_order, selected_genes = unique(top15$gene), gaps_col = "seurat_clusters", assay = 'integrated')
 graphics.off()
 
-saveRDS(norm.data.clustfilt, paste0(rds.path, "seurat_out.RDS"))
-
-# norm.data.clustfilt <- readRDS(paste0(rds.path, "seurat_out.RDS"))
-
-############################### Cluster without HH4 ########################################
-
-norm.data.hh4filt <- rownames(norm.data.clustfilt@meta.data)[grepl('hh4', rownames(norm.data.clustfilt@meta.data))]
-
-norm.data.hh4filt <- subset(norm.data.clustfilt, cells = norm.data.hh4filt, invert = T)
-
-# Re-run findvariablefeatures and scaling
-norm.data.hh4filt <- FindVariableFeatures(norm.data.hh4filt, selection.method = "vst", nfeatures = 2000, assay = 'RNA')
-
-# Enable parallelisation
-plan("multiprocess", workers = ncores)
-options(future.globals.maxSize = 2000 * 1024^2)
-
-norm.data.hh4filt <- ScaleData(norm.data.hh4filt, features = rownames(norm.data.hh4filt), vars.to.regress = c("percent.mt", "sex"))
-saveRDS(norm.data.hh4filt, paste0(rds.path, "norm.data.hh4filt.RDS"))
-
-# Read in RDS data if needed
-# norm.data.hh4filt <- readRDS(paste0(rds.path, "norm.data.hh4filt.RDS"))
-
-# Change plot path
-curr.plot.path <- paste0(plot.path, "4_hh4_filt/")
-dir.create(curr.plot.path)
-
-# PCA
-norm.data.hh4filt <- RunPCA(object = norm.data.hh4filt, verbose = FALSE)
-
-png(paste0(curr.plot.path, "dimHM.png"), width=30, height=50, units = 'cm', res = 200)
-DimHeatmap(norm.data.hh4filt, dims = 1:30, balanced = TRUE, cells = 500)
-graphics.off()
-
-png(paste0(curr.plot.path, "elbowplot.png"), width=24, height=20, units = 'cm', res = 200)
-print(ElbowPlot(norm.data.hh4filt, ndims = 40))
-graphics.off()
-
-png(paste0(curr.plot.path, "UMAP_PCA_comparison.png"), width=40, height=30, units = 'cm', res = 200)
-PCA.level.comparison(norm.data.hh4filt, PCA.levels = c(10, 20, 30, 40), cluster_res = 0.5)
-graphics.off()
-
-# Use PCA=15 as elbow plot is relatively stable across stages
-norm.data.hh4filt <- FindNeighbors(norm.data.hh4filt, dims = 1:30, verbose = FALSE)
-norm.data.hh4filt <- RunUMAP(norm.data.hh4filt, dims = 1:30, verbose = FALSE)
-
-# Find optimal cluster resolution
-png(paste0(curr.plot.path, "clustree.png"), width=70, height=35, units = 'cm', res = 200)
-clust.res(seurat.obj = norm.data.hh4filt, by = 0.2, prefix = "integrated_snn_res.")
-graphics.off()
-
-# Use clustering resolution = 1.2
-norm.data.hh4filt <- FindClusters(norm.data.hh4filt, resolution = 1.2)
-
-# Plot UMAP for clusters and developmental stage
-png(paste0(curr.plot.path, "UMAP.png"), width=40, height=20, units = 'cm', res = 200)
-clust.stage.plot(norm.data.hh4filt, stage.col = "stage")
-graphics.off()
-
-# Find differentially expressed genes and plot heatmap of top DE genes for each cluster
-markers <- FindAllMarkers(norm.data.hh4filt, only.pos = T, logfc.threshold = 0.25)
-# get automated cluster order based on percentage of cells in adjacent stages
-cluster.order = order.cell.stage.clust(seurat_object = norm.data.hh4filt, col.to.sort = seurat_clusters, sort.by = stage)
-# Re-order genes in top15 based on desired cluster order in subsequent plot - this orders them in the heatmap in the correct order
-top15 <- markers %>% group_by(cluster) %>% top_n(n = 15, wt = avg_logFC) %>% arrange(factor(cluster, levels = cluster.order))
-
-png(paste0(curr.plot.path, 'HM.top15.norm.data.hh4filt.png'), height = 75, width = 100, units = 'cm', res = 500)
-tenx.pheatmap(data = norm.data.hh4filt, metadata = c("seurat_clusters", "stage"), custom_order_column = "seurat_clusters",
-              custom_order = cluster.order, selected_genes = unique(top15$gene), gaps_col = "seurat_clusters", assay = 'integrated')
-graphics.off()
-
-saveRDS(norm.data.hh4filt, paste0(rds.path, "seurat_out_hh4filt.RDS"))
+saveRDS(poor_cluster_filt_data, paste0(rds_path, "poor_cluster_filt_data.RDS"))
