548230cd6b1b21180fa1da7e4bc4cba98ec0cb32
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-04-03 09:35:00 +0100

    debug process requirements

--- NF-downstream_analysis/bin/seurat/5_cell_cycle.R
+++ NF-downstream_analysis/bin/seurat/5_cell_cycle.R
@@ -114,6 +114,8 @@ print(gridExtra::grid.arrange(DimPlot(pre_cell_cycle_data, group.by = "Phase", r
 graphics.off()
 
 
+seurat_data <- DietSeurat(seurat_data, counts = TRUE, data = TRUE, scale.data = TRUE, assays = c('RNA', 'integrated'))
+
 # switch to RNA assay for viewing expression data
 DefaultAssay(cell_cycle_data) <- "RNA"
 
