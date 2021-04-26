d3fb1e257053bee31ea37fdc858482d6d0481705
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-04-26 13:20:21 +0100

    generate multiple phate plots with different params

--- NF-downstream_analysis/bin/other/phateR.R
+++ NF-downstream_analysis/bin/other/phateR.R
@@ -44,19 +44,113 @@ opt = getopt(spec)
   dir.create(plot_path, recursive = T)
 }
 
-seurat_data <- readRDS(paste0(data_path, 'contamination_filt_data.RDS'))
+seurat_data <- readRDS(paste0(data_path, 'contamination_filt_data_uncomp.RDS'))
 
 DefaultAssay(seurat_data) <- 'integrated'
 
-seurat_sub <- subset(seurat_data, cells = rownames(filter(seurat_data@meta.data, !grepl("hh4", orig.ident))))
+phate_dat <- t(GetAssayData(seurat_data, slot = "scale.data"))
 
-phate_dat <- t(GetAssayData(seurat_sub, slot = "scale.data"))
+phate_out <- phate(phate_dat)
 
-phate_out <- phate(phate_dat, knn = 10, decay = 100, t = 50)
-
-png(paste0(plot_path, "phateR.png"), width=20, height=20, units = 'cm', res = 200)
-ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_sub$orig.ident)) +
+png(paste0(plot_path, "phate_hh4_default.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
   geom_point() +
   labs(colour='Cell Type')
 graphics.off()
 
+phate_out <- phate(phate_dat, knn = 20)
+
+png(paste0(plot_path, "phate_knn20.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, t = 50)
+
+png(paste0(plot_path, "phate_t50.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, gamma = 0)
+
+png(paste0(plot_path, "phate_gamma0.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, knn = 20, t = 50)
+
+png(paste0(plot_path, "phate_knn20_t50.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, knn = 20, t = 50, gamma = 0)
+
+png(paste0(plot_path, "phate_knn20_t50_gamma0.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+
+
+
+
+
+seurat_data <- subset(seurat_data, cells = rownames(filter(seurat_data@meta.data, !grepl("hh4", orig.ident))))
+
+phate_dat <- t(GetAssayData(seurat_data, slot = "scale.data"))
+
+phate_out <- phate(phate_dat)
+
+png(paste0(plot_path, "phate_hh4_default.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, knn = 20)
+
+png(paste0(plot_path, "phate_hh4_knn20.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, t = 50)
+
+png(paste0(plot_path, "phate_hh4_t50.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, gamma = 0)
+
+png(paste0(plot_path, "phate_hh4_gamma0.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, knn = 20, t = 50)
+
+png(paste0(plot_path, "phate_hh4_knn20_t50.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
+
+phate_out <- phate(phate_dat, knn = 20, t = 50, gamma = 0)
+
+png(paste0(plot_path, "phate_hh4_knn20_t50_gamma0.png"), width=20, height=20, units = 'cm', res = 200)
+ggplot(phate_out, aes(PHATE1, PHATE2, colour = seurat_data$orig.ident)) +
+  geom_point() +
+  labs(colour='Cell Type')
+graphics.off()
