7ab379cb52d0f1959f28f8f76005a096e20773c5
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-06-03 16:18:53 +0100

    update gene modules script

--- NF-downstream_analysis/bin/other/gene_modules.R
+++ NF-downstream_analysis/bin/other/gene_modules.R
@@ -20,31 +20,31 @@ opt = getopt(spec)
 {
   if(length(commandArgs(trailingOnly = TRUE)) == 0){
     cat('No command line arguments provided, paths are set for running interactively in Rstudio server\n')
-
-    plot_path = "./output/NF-downstream_analysis/gene_modules/plots/"
-    rds_path = "./output/NF-downstream_analysis/gene_modules/rds_files/"
-    antler_path = "./output/NF-downstream_analysis/gene_modules/antler_data/"
-    data_path = "./output/NF-downstream_analysis/seurat/6_contamination_filt/rds_files/"
+    
+    plot_path = "./output/NF-downstream_analysis_stacas/gene_modules/plots/"
+    rds_path = "./output/NF-downstream_analysis_stacas/gene_modules/rds_files/"
+    antler_path = "./output/NF-downstream_analysis_stacas/gene_modules/antler_data/"
+    data_path = "./output/NF-downstream_analysis_stacas/seurat/6_contamination_filt/rds_files/"
     
     ncores = 8
-
+    
   } else if (opt$runtype == "nextflow"){
     cat('pipeline running through Nextflow\n')
-
+    
     plot_path = "./plots/"
     rds_path = "./rds_files/"
     antler_path = "./antler_data/"
     data_path = "./input/rds_files/"
     ncores = opt$cores
-
+    
     # Multi-core when running from command line
     plan("multiprocess", workers = ncores)
     options(future.globals.maxSize = 16* 1024^3) # 32gb
-
+    
   } else {
     stop("--runtype must be set to 'nextflow'")
   }
-
+  
   cat(paste0("script ran with ", ncores, " cores\n"))
   dir.create(plot_path, recursive = T)
   dir.create(rds_path, recursive = T)
@@ -64,8 +64,6 @@ seurat_data <- DietSeurat(seurat_data, counts = TRUE, data = TRUE, scale.data = 
 
 # strip end of cell names as this is incorrectly reformated in Antler
 seurat_data <- RenameCells(seurat_data, new.names = gsub('-', '_', colnames(seurat_data)))
-
-# seurat_data <- RenameCells(seurat_data, new.names = sub('-.*', '', colnames(seurat_data)))
 
 antler_data <- data.frame(row.names = colnames(seurat_data),
                           "timepoint" = as.numeric(substring(colnames(seurat_data), 3, 3)),
@@ -90,8 +88,10 @@ antler_data$exclude_unexpressed_genes(min_cells=10, min_level=1, verbose=T, data
 
 antler_data$normalize(method = 'MR')
 
+########################################################################################################################################################
+# Calculate 200 GMs
 antler_data$gene_modules$identify(
-  name                  = "unbiasedGMs",
+  name                  = "GMs200",
   corr_t                = 0.3,  # the Spearman correlation treshold
   corr_min              = 3,    # min. number of genes a gene must correlate with
   mod_min_cell          = 10,   # min. number of cells expressing the module
@@ -99,33 +99,35 @@ antler_data$gene_modules$identify(
   num_initial_gms       = 200,
   process_plots         = TRUE)
 
-saveRDS(antler_data, paste0(rds_path, "antler_all.RDS"))
-# antler <- readRDS(paste0(rds_path, "antler_all.RDS"))
-
 # plot all gene modules
-png(paste0(plot_path, 'allmodules_200.png'), height = 100, width = 80, units = 'cm', res = 400)
-GeneModulePheatmap(seurat_obj = seurat_data, metadata = c("stage", "seurat_clusters"), gene_modules = antler_data$gene_modules$lists$unbiasedGMs$content,
-        show_rownames = F, col_order = c("stage", "seurat_clusters"))
+ncell = ncol(seurat_data)
+ngene = length(unlist(antler_data$gene_modules$lists$GMs200$content))
+
+metadata = c("stage", "seurat_clusters", "run")
+
+png(paste0(plot_path, 'allmodules_200.png'), height = round(ngene/10), width = 75, units = 'cm', res = 400)
+GeneModulePheatmap(seurat_obj = seurat_data, metadata = metadata, gene_modules = antler_data$gene_modules$lists$GMs200$content,
+                   show_rownames = FALSE, col_order = metadata, col_ann_order = metadata, gaps_col = "stage", fontsize = 13)
 graphics.off()
 
-# # use bait genes to filter mods
-# bait_genes = c("PAX7", "SOX2", "SOX21", "SOX10", "EYA2", "GBX2", "PAX6", "PAX2", "SIX3", "FRZB", "MSX1", "WNT1", "DLX5", "TFAP2A", "TFAP2B", "AXUD1", "GATA2", "HOMER2", "SIX1", "EYA2", "ETS1")
-# temp_gms = lapply(antler$gene_modules$lists$unbiasedGMs$content, function(x) if(any(bait_genes %in% x)){x})
-# 
-# png(paste0(plot_path, 'DE.GM.200.png'), height = 50, width = 80, units = 'cm', res = 400)
-# GeneModulePheatmap(data = seurat_data, metadata = c("stage", "orig.ident", "seurat_clusters"), gene_modules = temp_gms, gaps_col = "stage",
-#         show_rownames = T, col_order = c("stage", "seurat_clusters"))
-# graphics.off()
-# 
 
+# use bait genes to filter mods
+bait_genes = c("PAX7", "SOX2", "SOX21", "SOX10", "EYA2", "GBX2", "PAX6", "PAX2", "SIX3", "FRZB", "MSX1", "WNT1", "DLX5", "TFAP2A", "TFAP2B", "AXUD1", "GATA2", "HOMER2", "SIX1", "EYA2", "ETS1")
+temp_gms <- lapply(antler_data$gene_modules$lists$GMs200$content, function(x) if(any(bait_genes %in% x)){x})
+temp_gms <- temp_gms[!sapply(temp_gms, is.null)]
 
+ncell = ncol(seurat_data)
+ngene = length(unlist(temp_gms))
 
-
+png(paste0(plot_path, 'bait_allmodules_200.png'), height = round(ngene/2), width = 75, units = 'cm', res = 400)
+GeneModulePheatmap(seurat_obj = seurat_data, metadata = metadata, gene_modules = temp_gms,
+                   show_rownames = TRUE, col_order = metadata, col_ann_order = metadata, gaps_col = "stage", fontsize = 25, fontsize_row = 18)
+graphics.off()
 
 # Plot gene modules with at least 50% of genes DE > 0.25 logFC & FDR < 0.001
 
 # Find DEGs
-DE_genes <- FindAllMarkers(seurat_data, only.pos = T, logfc.threshold = 0.25) %>% filter(p_val_adj < 0.001)
+DE_genes <- FindAllMarkers(seurat_data, only.pos = TRUE, logfc.threshold = 0.25) %>% filter(p_val_adj < 0.001)
 
 # Get automated cluster order based on percentage of cells in adjacent stages
 cluster_order <- OrderCellClusters(seurat_object = seurat_data, col_to_sort = seurat_clusters, sort_by = stage)
@@ -133,16 +135,14 @@ cluster_order <- OrderCellClusters(seurat_object = seurat_data, col_to_sort = se
 # Filter GMs with 50% genes DE logFC > 0.25 & FDR < 0.001
 gms <- SubsetGeneModules(antler_data$gene_modules$get("unbiasedGMs"), selected_genes = DE_genes$gene, keep_mod_ID = T, selected_gene_ratio = 0.5)
 
-png(paste0(plot_path, 'DE_gms.png'), height = 160, width = 80, units = 'cm', res = 500)
-GeneModulePheatmap(seurat_obj = seurat_data, metadata = c("stage", "seurat_clusters"), gene_modules = gms, gaps_col = "seurat_clusters", custom_order = cluster_order, custom_order_column = "seurat_clusters", fontsize = 25, fontsize_row = 10)
+png(paste0(plot_path, 'DE_allmodules_200.png'), height = round(ngene/2), width = 75, units = 'cm', res = 400)
+GeneModulePheatmap(seurat_obj = seurat_data,  metadata = metadata, gene_modules = gms, custom_order = cluster_order, custom_order_column = "seurat_clusters",
+                   show_rownames = TRUE, col_order = metadata, col_ann_order = metadata, gaps_col = "stage", fontsize = 15, fontsize_row = 10)
 graphics.off()
 
 
-
-
-
-
-
+########################################################################################################################################################
+# Calculate GMs unbiasedly
 antler_data$gene_modules$identify(
   name                  = "unbiasedGMs",
   corr_t                = 0.3,  # the Spearman correlation treshold
@@ -151,19 +151,44 @@ antler_data$gene_modules$identify(
   mod_consistency_thres = 0.4,  # ratio of expressed genes among "positive" cells
   process_plots         = TRUE)
 
-saveRDS(antler_data, paste0(rds_path, "antler_all.RDS"))
-# antler <- readRDS(paste0(rds_path, "antler_all.RDS"))
-
 # plot all gene modules
-png(paste0(plot_path, 'allmodules_unbiased.png'), height = 100, width = 80, units = 'cm', res = 400)
-GeneModulePheatmap(seurat_obj = seurat_data, metadata = c("stage", "seurat_clusters"), gene_modules = antler_data$gene_modules$lists$unbiasedGMs$content,
-        show_rownames = F, col_order = c("stage", "seurat_clusters"))
+ncell = ncol(seurat_data)
+ngene = length(unlist(antler_data$gene_modules$lists$unbiasedGMs$content))
+
+metadata = c("stage", "seurat_clusters", "run")
+
+png(paste0(plot_path, 'allmodules_unbiased.png'), height = round(ngene/10), width = 75, units = 'cm', res = 400)
+GeneModulePheatmap(seurat_obj = seurat_data, metadata = metadata, gene_modules = antler_data$gene_modules$lists$unbiasedGMs$content,
+                   show_rownames = FALSE, col_order = metadata, col_ann_order = metadata, gaps_col = "stage", fontsize = 15)
 graphics.off()
+
 
 # use bait genes to filter mods
 bait_genes = c("PAX7", "SOX2", "SOX21", "SOX10", "EYA2", "GBX2", "PAX6", "PAX2", "SIX3", "FRZB", "MSX1", "WNT1", "DLX5", "TFAP2A", "TFAP2B", "AXUD1", "GATA2", "HOMER2", "SIX1", "EYA2", "ETS1")
-temp_gms = lapply(antler_data$gene_modules$lists$unbiasedGMs$content, function(x) if(any(bait_genes %in% x)){x})
+temp_gms <- lapply(antler_data$gene_modules$lists$unbiasedGMs$content, function(x) if(any(bait_genes %in% x)){x})
+temp_gms <- temp_gms[!sapply(temp_gms, is.null)]
 
-png(paste0(plot_path, 'allmodules_unbiased_bait.png'), height = 50, width = 80, units = 'cm', res = 400)
-GeneModulePheatmap(seurat_obj = seurat_data, metadata = c("stage", "orig.ident", "seurat_clusters"), col_order = c("stage", "seurat_clusters"), gene_modules = temp_gms, gaps_col = "stage")
+ncell = ncol(seurat_data)
+ngene = length(unlist(temp_gms))
+
+png(paste0(plot_path, 'bait_allmodules_unbiased.png'), height = round(ngene/2), width = 75, units = 'cm', res = 400)
+GeneModulePheatmap(seurat_obj = seurat_data, metadata = metadata, gene_modules = temp_gms,
+                   show_rownames = TRUE, col_order = metadata, col_ann_order = metadata, gaps_col = "stage", fontsize = 15, fontsize_row = 10)
+graphics.off()
+
+
+# Plot gene modules with at least 50% of genes DE > 0.25 logFC & FDR < 0.001
+
+# Find DEGs
+DE_genes <- FindAllMarkers(seurat_data, only.pos = TRUE, logfc.threshold = 0.25) %>% filter(p_val_adj < 0.001)
+
+# Get automated cluster order based on percentage of cells in adjacent stages
+cluster_order <- OrderCellClusters(seurat_object = seurat_data, col_to_sort = seurat_clusters, sort_by = stage)
+
+# Filter GMs with 50% genes DE logFC > 0.25 & FDR < 0.001
+gms <- SubsetGeneModules(antler_data$gene_modules$get("unbiasedGMs"), selected_genes = DE_genes$gene, keep_mod_ID = T, selected_gene_ratio = 0.5)
+
+png(paste0(plot_path, 'DE_allmodules_unbiased.png'), height = round(ngene/2), width = 75, units = 'cm', res = 400)
+GeneModulePheatmap(seurat_obj = seurat_data,  metadata = metadata, gene_modules = gms, custom_order = cluster_order, custom_order_column = "seurat_clusters",
+                   show_rownames = TRUE, col_order = metadata, col_ann_order = metadata, gaps_col = "stage", fontsize = 15, fontsize_row = 10)
 graphics.off()
