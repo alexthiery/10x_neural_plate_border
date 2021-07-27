aa6a19a00e1984b77d4f7faa877e663b7bcbeab1
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-07-27 17:41:34 +0100

    remove misc slot before converting to h5Seurat in order to circumvent recursion error

--- NF-downstream_analysis/modules/local/seurat_h5ad/bin/seurat_h5ad.R
+++ NF-downstream_analysis/modules/local/seurat_h5ad/bin/seurat_h5ad.R
@@ -22,10 +22,20 @@ if(opt$verbose) print(opt)
 cat('pipeline running through Nextflow\n')
 data_path = "./input/rds_files/"
 
+# Options for testing
+# opt <- list()
+# opt$assay = 'integrated'
+# opt$outfile = 'seurat'
+# opt$group_by = 'seurat_clusters'
+# seurat_object <- readRDS("~/output/NF-downstream_analysis_stacas/seurat/6_contamination_filt/rds_files/contamination_filt_data.RDS")
+
 seurat_object <- readRDS(list.files(data_path, full.names = TRUE, recursive = TRUE))
 
 DefaultAssay(seurat_object) <- opt$assay
 seurat_object <- DietSeurat(seurat_object, counts = TRUE, assays = opt$assay, dimreducs = c('pca', 'umap'))
+
+# remove anything from misc slot as depending on the contents this can cause recursion errors
+seurat_test@misc <- list()
 
 # if --group_by is specified, generate cell colours gor group_by column
 if(!is.null(opt[['group_by']]) && opt[['group_by']] %in% colnames(seurat_object@meta.data)){
@@ -47,8 +57,11 @@ if(!is.null(opt[['group_by']]) && opt[['group_by']] %in% colnames(seurat_object@
 }
 
 # SaveH5Seurat sometimes encounters a recursion error. File is already written by this point so error can be ignored with try().
-try(SaveH5Seurat(seurat_object, filename = paste0(opt$outfile, '.h5Seurat')), silent = TRUE)
+# try(SaveH5Seurat(seurat_object, filename = paste0(opt$outfile, '.h5Seurat')), silent = TRUE)
+SaveH5Seurat(seurat_object, filename = paste0(opt$outfile, '.h5Seurat'))
 Convert(paste0(opt$outfile, '.h5Seurat'), dest = "h5ad")
 
 # Remove intermediate h5Seurat file
 file.remove(paste0(opt$outfile, '.h5Seurat'))
+
+
