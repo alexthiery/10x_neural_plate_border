2e11a9acb3cd994fd277181390a7f2b806f4ab3a
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-07-29 15:04:06 +0100

    DEBUG

--- NF-downstream_analysis/bin/seurat/subset_cluster.R
+++ NF-downstream_analysis/bin/seurat/subset_cluster.R
@@ -34,7 +34,7 @@ options(future.globals.maxSize = 16* 1024^3) # 32gb
 # Set paths and load data
 plot_path = "./plots/"
 rds_path = "./rds_files/"
-data_path = "./input/rds_files/"
+data_path = "./input/"
 
 dir.create(plot_path, recursive = T)
 dir.create(rds_path, recursive = T)
