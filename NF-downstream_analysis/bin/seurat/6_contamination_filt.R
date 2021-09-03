4719423867ebcf0379b99cdc387c6599c250694c
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-09-03 17:01:42 +0100

    tweak plot sizes

--- NF-downstream_analysis/bin/seurat/6_contamination_filt.R
+++ NF-downstream_analysis/bin/seurat/6_contamination_filt.R
@@ -150,7 +150,7 @@ ClustStagePlot(contamination_filt_data, stage_col = "stage")
 graphics.off()
 
 # Check integration on final filtered data
-png(paste0(plot_path, "CheckIntegration.png"), width = 25, height = 10, res = 200, units = "cm")
+png(paste0(plot_path, "CheckIntegration.png"), width = 45, height = 15, res = 200, units = "cm")
 CheckIntegration(contamination_filt_data, xlim = c(-10, 10), ylim = c(-10, 10))
 graphics.off()
 
