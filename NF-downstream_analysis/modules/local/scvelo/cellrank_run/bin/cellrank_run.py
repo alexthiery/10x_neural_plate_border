141ab9f62583faedf57dfc78ea44b229476bf186
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-09-27 14:03:22 +0100

    misc

--- NF-downstream_analysis/modules/local/scvelo/cellrank_run/bin/cellrank_run.py
+++ NF-downstream_analysis/modules/local/scvelo/cellrank_run/bin/cellrank_run.py
@@ -62,7 +62,7 @@ def write_lineage_probs(adata):
 
 def allDataTerminalStates(adata, estimator):
     estimator.set_terminal_states({"neural": adata[adata.obs["scHelper_cell_type"].isin(['hindbrain', 'midbrain', "forebrain"]) & adata.obs["stage"].isin(['ss4', 'ss8'])].obs_names,
-                  "NC": adata[adata.obs["scHelper_cell_type"].isin(['NC']) & adata.obs["stage"].isin(['ss4', 'ss8'])].obs_names,
+                  "NC": adata[adata.obs["scHelper_cell_type"].isin(['delaminating_NC']) & adata.obs["stage"].isin(['ss4', 'ss8'])].obs_names,
                   "placodal": adata[adata.obs["scHelper_cell_type"].isin(['aPPR', 'pPPR']) & adata.obs["stage"].isin(['ss4', 'ss8'])].obs_names})
     cr.pl.terminal_states(adata, save='terminal_states.pdf')
     return(estimator)
