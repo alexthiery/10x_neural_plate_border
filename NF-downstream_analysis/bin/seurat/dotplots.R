027cb9d812ea0d7ad79c201fe831fd4c200db6d5
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-11-19 15:09:28 +0000

    edit genes in dotplots

--- NF-downstream_analysis/bin/seurat/dotplots.R
+++ NF-downstream_analysis/bin/seurat/dotplots.R
@@ -51,63 +51,11 @@ opt = getopt(spec)
   dir.create(rds_path, recursive = T)
 }
 
-# Retrieve seurat object label
-label <- sub('_.*', '', list.files(data_path, pattern = '*.RDS'))
-
 seurat_data <- readRDS(list.files(data_path, full.names = TRUE, pattern = '*.RDS'))
-hh4_data <- readRDS('./output/NF-downstream_analysis_stacas/stage_split/hh4_splitstage_data/seurat/stage_state_classification/rds_files/hh4_cell_state_classification.RDS')
-hh5_data <- readRDS('./output/NF-downstream_analysis_stacas/stage_split/hh5_splitstage_data/seurat/stage_state_classification/rds_files/hh5_cell_state_classification.RDS')
-# seurat_data <- readRDS('./output/NF-downstream_analysis_stacas/stage_split/hh6_splitstage_data/seurat/stage_cluster/rds_files/hh6_clustered_data.RDS')
-# seurat_data <- readRDS('./output/NF-downstream_analysis_stacas/stage_split/hh7_splitstage_data/seurat/stage_cluster/rds_files/hh7_clustered_data.RDS')
-# seurat_data <- readRDS('./output/NF-downstream_analysis_stacas/stage_split/ss4_splitstage_data/seurat/stage_cluster/rds_files/ss4_clustered_data.RDS')
-# seurat_data <- readRDS('./output/NF-downstream_analysis_stacas/stage_split/ss8_splitstage_data/seurat/stage_cluster/rds_files/ss8_clustered_data.RDS')
-
-
-DimPlot(seurat_data, group.by = 'scHelper_cell_type')
-
-meta_col = 'scHelper_cell_type'
-if(meta_col == 'scHelper_cell_type'){
-  scHelper_cell_type_order <- c('extra_embryonic', 'NNE', 'prospective_epidermis', 'PPR', 'aPPR', 'pPPR', 'early_NPB', 'NPB',
-                                'aNPB', 'pNPB', 'NC', 'delaminating_NC', 'early_neural', 'early_caudal_neural', 'NP', 'pNP',
-                                'hindbrain', 'iNP', 'midbrain', 'aNP', 'forebrain', 'ventral_forebrain', 'node', 'streak')
-  
-  scHelper_cell_type_order <- scHelper_cell_type_order[scHelper_cell_type_order %in% unique(seurat_data@meta.data[[meta_col]])]
-  
-  if(!all(seurat_data@meta.data$scHelper_cell_type %in% scHelper_cell_type_order)){
-    stop('Check scHelper_cell_type_order. Cell types found within "seurat_data@meta.data$scHelper_cell_type" are missing from custom order vector')
-  }
-  
-  seurat_data@meta.data$scHelper_cell_type <- factor(seurat_data@meta.data$scHelper_cell_type, levels = scHelper_cell_type_order)
-}
-
-Idents(seurat_data) <- meta_col
-
-
-data_subset <- subset(seurat_data, cells = rownames(filter(seurat_data@meta.data, scHelper_cell_type %in% c('NNE', 'early_NPB', 'early_neural'))))
-
-data_subset <- subset(seurat_data, cells = rownames(filter(seurat_data@meta.data, scHelper_cell_type %in% c('NNE', 'aPPR', 'pNPB', 'pNP', 'iNP', 'aNP'))))
-
-
-goi <- list(hh4 = c('DLX5', 'GATA2', 'MSX1', 'BMP4', 'TFAP2A', 'TFAP2C', 'PAX7', 'SOX3', 'OTX2', 'MAFA', 'FRZB'),
-            hh5 = c('SOX21', 'SIX3', 'OTX2', 'GBX2', 'BTG2', 'MSX1', 'PAX7', 'TFAP2B', 'SIX1', 'EYA2', 'TFAP2C', 'DLX5', 'BMP4'))
-
-
-DotPlot(hh4_data, features = genes) + 
-  theme(axis.text.x = element_text(angle = 90)) + coord_flip()
-
-
-
-
-seurat_data@misc <- data.frame(temp = 1:10)
-
-
 seurat_data <- readRDS('./output/NF-downstream_analysis_stacas/transfer_labels/seurat/rds_files/seurat_label_transfer.RDS')
 
 
-
-
-
-dotplot <- function(data, cells, genes, group_by, facet, group_by_levels=NULL, facet_dir = 'h'){
+dotplot <- function(data, cells, genes, group_by, facet, group_by_levels=NULL, facet_dir = 'h', limits = c(0, NA), range = c(0,8)){
   
   if(is.null(group_by_levels)){
     group_by_levels <- unique(seurat_data@meta.data[cells, group_by])
@@ -136,7 +84,7 @@ dotplot <- function(data, cells, genes, group_by, facet, group_by_levels=NULL, f
   
   plot <- ggplot(temp, aes(x = !!sym(group_by), y = gene, colour = average_expression)) +
     geom_point(aes(size = percent_expressed)) +
-    scale_radius(limits = c(0, NA), range = c(0, 8)) +
+    scale_radius(limits = limits, range = range) +
     theme_bw() +
     theme(panel.grid.major = element_blank()) +
     scale_colour_gradient(low = 'gray90', high = 'blue') +
@@ -149,35 +97,37 @@ dotplot <- function(data, cells, genes, group_by, facet, group_by_levels=NULL, f
   } else if(facet_dir == 'v'){
     plot <- plot + facet_wrap(~ stage, dir = 'v', ncol = 1)
   }
-    
+  
   return(plot)
 }
 
 
 
 
-
-genes <- c('SOX21', 'SIX3', 'OTX2', 'GBX2', 'BTG2', 'MSX1', 'PAX7', 'TFAP2B', 'SIX1', 'EYA2', 'TFAP2C', 'DLX5', 'BMP4')
 cell_type_order <- c('extra_embryonic', 'NNE', 'prospective_epidermis', 'PPR', 'aPPR', 'pPPR', 'early_NPB', 'NPB',
                      'aNPB', 'pNPB', 'NC', 'delaminating_NC', 'early_neural', 'early_caudal_neural', 'NP', 'pNP',
                      'hindbrain', 'iNP', 'midbrain', 'aNP', 'forebrain', 'ventral_forebrain', 'node', 'streak')
 
 
-
+genes <- rev(c('EPAS1', 'GATA3', 'SIX1', 'EYA2', 'DLX5', 'BMP4', 'MSX1', 'TFAP2A', 'TFAP2B', 'PAX7', 'SOX2', 'OTX2', 'YEATS4', 'SOX21', 'GBX2', 'SIX3', 'ADMP', 'EOMES'))
 cells <- rownames(filter(seurat_data@meta.data, stage %in% c('hh4', 'hh5', 'hh6')))
 
-dotplot(seurat_data, cells, genes, group_by = 'scHelper_cell_type', facet = 'stage', group_by_levels = cell_type_order)
+png(paste0(plot_path, 'hh4-hh6_dotplot.png'), width = 23, height = 13, units = 'cm', res = 400)
+dotplot(seurat_data, cells, genes, group_by = 'scHelper_cell_type', facet = 'stage', group_by_levels = cell_type_order, limits = c(5, NA), range = c(0,6)) +
+  guides(size=guide_legend(title="Percent cells expressing"), colour = guide_colourbar(title="Average expression")) +
+  theme(strip.text.x = element_text(size=15),
+        axis.text.x=element_text(size=12),
+        axis.text.y=element_text(size=11))
+graphics.off()
 
 
-cells <- rownames(filter(seurat_data@meta.data,scHelper_cell_type %in% c('NP', 'pNP','hindbrain', 'iNP', 'midbrain', 'aNP', 'forebrain', 'early_caudal_neural')))
-genes <- c('SIX3', 'OTX2', 'GBX2')
+genes <- rev(c('GATA3', 'DLX5', 'SIX1', 'EYA2', 'MSX1', 'TFAP2A', 'TFAP2B', 'Pax3', 'PAX7', 'CSRNP1', 'ENSGALG00000030902', 'SOX10', 'SOX2', 'SOX21', 'GBX2', 'PAX2', 'SIX3'))
+cells <- rownames(filter(seurat_data@meta.data, stage %in% c('hh7', 'ss4', 'ss8')))
 
-dotplot(seurat_data, cells=cells, genes, group_by = 'scHelper_cell_type', facet = 'stage', group_by_levels = cell_type_order, facet_dir = 'h')
-
-
-
-
-
-
-
-
+png(paste0(plot_path, 'hh7-ss8_dotplot.png'), width = 23, height = 13, units = 'cm', res = 400)
+dotplot(seurat_data, cells, genes, group_by = 'scHelper_cell_type', facet = 'stage', group_by_levels = cell_type_order, limits = c(5, NA), range = c(0,6)) +
+  guides(size=guide_legend(title="Percent cells expressing"), colour = guide_colourbar(title="Average expression")) +
+  theme(strip.text.x = element_text(size=15),
+        axis.text.x=element_text(size=12),
+        axis.text.y=element_text(size=11))
+graphics.off()
