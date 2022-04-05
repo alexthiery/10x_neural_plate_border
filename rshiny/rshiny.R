c319a322db5882c9ccebc466dc99c0402edc2d14
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2022-04-05 12:52:32 +0100

    add feature plots for coexpression tab

--- rshiny/rshiny.R
+++ rshiny/rshiny.R
@@ -5,125 +5,18 @@ library(tidyverse)
 library(viridis)
 library(mgcv)
 
+source('./custom_functions.R')
 
 # dat <- readRDS('../shiny_test.RDS')
 dat_list <- readRDS('./output/rshiny_input.RDS')
 
 
 
+# Set parameters
+lineage_map = c('Neural' = 'lineage_neural_probability', 'Neural crest' = 'lineage_NC_probability', 'Placodal' = 'lineage_placodal_probability')
+scvelo_features = c('latent_time', 'lineage_neural_probability', 'lineage_NC_probability', 'lineage_placodal_probability')
 
 
-
-calc_latent_time_cutoff <- function(latent_time, lineage_probability, top_frac = 0.2, return = 'intercept'){
-  data = data.frame(latent_time, lineage_probability)
-  
-  model_data <- data %>%
-    filter(lineage_probability > 0 & lineage_probability < 1) %>%
-    filter(lineage_probability > quantile(lineage_probability, 1-top_frac)) %>%
-    # If cells remaining in top frac have less than 0.5 probability of giving rise to the lineage then remove (required for short lineages)
-    filter(lineage_probability > 0.5)
-  
-  # Fit model to top frac of data
-  fit = lm(lineage_probability ~ latent_time, data = model_data)
-  
-  # Inverse equation to find X for Y = 1
-  x <- (1-coef(fit)[1])/coef(fit)[2]
-  
-  # Identify max latent time value based on cells which have reached lineage probability == 1
-  max_latent_time <- data %>% filter(lineage_probability == 1) %>% filter(latent_time == max(latent_time)) %>% dplyr::pull(latent_time)
-  
-  if(return == 'plot'){
-    p = ggplot(data, aes(latent_time, lineage_probability)) + 
-      geom_point(size = 0.1) +
-      geom_abline(intercept = coef(fit)[1], slope = coef(fit)[2])
-    
-    return(p)
-  }else if(return == 'intercept'){
-    if(x > max_latent_time){
-      cat('Predicted latent time is later than any cell observed that has reached full lineage absorbtion. Max latent time is set based on oldest cell at lineage_probability == 1')
-      return(max_latent_time)
-    }else{
-      return(unname(x))
-    }
-  }else{
-    stop('return must be one of intercept or plot')
-  }
-}
-
-calc_gams <- function(dynamics_data){
-  # GAMs
-  gams <- dynamics_data %>%
-    group_by(lineage) %>%
-    do(gams = gam(scaled_expression ~ s(latent_time, bs = "cs", k=5), weights = lineage_probability, data = .))
-  
-  # Add module column and max latent time to gam data
-  extra_dat <- dynamics_data %>%
-    ungroup() %>%
-    dplyr::select(gene, lineage, max_latent_time) %>%
-    distinct()
-  
-  gams <- inner_join(gams, extra_dat)
-  
-  # Generate predicted values for each gam in tidy df -> output in long format
-  plot_data <- data.frame()
-  for(row in 1:nrow(gams)){
-    # Generate latent time values to predict gams on -> use max latent_time calculated per lineage
-    pdat <- tibble(latent_time = seq(0, gams[[row, 'max_latent_time']], length = 100))
-    new_data <- predict.gam(gams[[row,'gams']][[1]], newdata = pdat, se=TRUE)
-    
-    plot_data <- rbind(plot_data, data.frame(gene = gams[[row, 'gene']],
-                                             lineage = gams[[row, 'lineage']],
-                                             scaled_expression = new_data[['fit']],
-                                             se = new_data[['se.fit']],
-                                             pdat))
-  }
-  return(plot_data)
-}
-
-
-
-# Function for generating dataframe for gene expression dynamics
-get_lineage_expression_data <- function(gene, seurat_object, assay = 'RNA', slot = 'data', lineages, lineage_cutoffs = NULL){
-  
-  if(!all(names(lineage_cutoffs) %in% lineages)){
-    stop('lineage_cutoffs names do not match selected lineages')
-  }
-  
-  assay_dat <- as.data.frame(GetAssayData(seurat_object, assay = 'RNA', slot = 'data')[gene,, drop=FALSE])
-  
-  subset_lineage_probabilities <- c('Neural' = 'lineage_neural_probability', 'Neural crest' = 'lineage_NC_probability', 'Placodal' = 'lineage_placodal_probability')
-  
-  cols_to_sub <- c('latent_time', unname(subset_lineage_probabilities[lineages]))
-  
-  assay_dat <- seurat_object@meta.data %>%
-    dplyr::select(cols_to_sub) %>%
-    merge(t(assay_dat), ., by = 0)
-  
-  plot_data <- assay_dat %>%
-    column_to_rownames('Row.names') %>%
-    pivot_longer(!all_of(cols_to_sub)) %>%
-    rename(scaled_expression = value) %>%
-    rename(gene = name) %>%
-    pivot_longer(cols = !c(latent_time, gene, scaled_expression)) %>%
-    rename(lineage_probability = value) %>%
-    rename(lineage = name) %>%
-    group_by(lineage) %>%
-    mutate(lineage = names(subset_lineage_probabilities)[subset_lineage_probabilities %in% lineage])
-  
-  # Filter expression values which are above the predicted lineage cutoffs 
-  if(!is.null(lineage_cutoffs)){
-    plot_data <- plot_data %>%
-      mutate(max_latent_time = lineage_cutoffs[names(lineage_cutoffs) %in% lineage]) %>%
-      filter(latent_time < max_latent_time)
-  }
-  
-  return(plot_data)
-}
-
-
-
-
-scvelo_features <- c('latent_time', 'lineage_neural_probability', 'lineage_NC_probability', 'lineage_placodal_probability')
 
 # Get shared var features for optional input
 gene_ids <- lapply(dat_list, VariableFeatures) %>% Reduce(c, .) %>% unique()
@@ -176,11 +69,34 @@ tab_lineage_dynamics <- tabItem(tabName = "lineage_dynamics",
                                            checkboxGroupInput("select_lineage", "Select lineages of interest", inline = TRUE),
                                            selectInput("gene_id_lineage_dynamics", "Select Gene", gene_ids, width = "200", selected = 'PAX7'),
                                            plotOutput("lineage_dynamics"),
-                                           # verbatimTextOutput("lineage_dynamics"),
                                            width = 12
                                          )
                                   )
                                 )
+)
+
+tab_coexpression_umaps <- tabItem(tabName = "coexpression_umaps",
+                             fluidRow(
+                               column(11,
+                                      radioButtons("coexpression_subset_featureplots", "Select dataset to visualise", names(dat_list), inline = TRUE, selected = 'Full data')
+                               )
+                             ),
+                             fluidRow(
+                               column(5, offset = 1,
+                                      box(
+                                        selectInput("coexpression_gene_id_1", "Select Gene 1", gene_ids, width = "200", selected = 'PAX7'),
+                                        plotOutput("coexpression_featureplot_1"),
+                                        width = 12
+                                      )
+                               ),
+                               column(5, offset = 1,
+                                      box(
+                                        selectInput("coexpression_gene_id_2", "Select Gene 2", gene_ids, width = "200", selected = 'SIX1'),
+                                        plotOutput("coexpression_featureplot_2"),
+                                        width = 12
+                                      )
+                               )
+                             )
 )
 
 
@@ -191,7 +107,8 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Home", tabName = "home"),
       menuItem("Feature Plots", tabName = "featureplots"),
-      menuItem("Lineage Dynamics", tabName = "lineage_dynamics")
+      menuItem("Lineage Dynamics", tabName = "lineage_dynamics"),
+      menuItem("UMAP co-expression", tabName = "coexpression_umaps")
     )
   ),
   
@@ -199,12 +116,11 @@ ui <- dashboardPage(
     tabItems(
       tab_home,
       tab_feature_plots,
-      tab_lineage_dynamics
+      tab_lineage_dynamics,
+      tab_coexpression_umaps
     )
   )
 )
-
-lineage_map = c('Neural' = 'lineage_neural_probability', 'Neural crest' = 'lineage_NC_probability', 'Placodal' = 'lineage_placodal_probability')
 
 
 
@@ -225,10 +141,6 @@ server <- function(input, output, session){
   
   ####################################################################
   # Generate scvelo latent time/lineage probability plots 
-  # scvelo_umap <- reactive({FeaturePlot(dat_list[[input$select_lineage_dataset]], features = 'lineage_neural_probability', pt.size = 1, order = FALSE) +
-  #                            scale_colour_viridis(option="viridis")})
-  
-  
   
   # Set lineage options based on dataset selected
   observe({
@@ -296,9 +208,7 @@ server <- function(input, output, session){
     )
   })
   
-  linegae_expression_data <- reactive(get_lineage_expression_data(input$gene_id_lineage_dynamics, subset_dataset(), lineages = input$select_lineage, lineage_cutoffs = max_cutoffs()))
-
-  plot_data <- reactive(calc_gams(linegae_expression_data()))
+  plot_data <- reactive(lineage_gam(input$gene_id_lineage_dynamics, subset_dataset(), slot = 'scale.data', lineages = input$select_lineage, lineage_cutoffs = max_cutoffs()))
   
   output$lineage_dynamics <- renderPlot(ggplot(plot_data(), aes(x = latent_time, y = scaled_expression, colour = lineage, fill = lineage)) +
     geom_line(size = 2) +
@@ -314,6 +224,13 @@ server <- function(input, output, session){
           legend.text=element_text(size=20))
   )
   
+
+  ####################################################################
+  # Co-expression feature plots
+  output$coexpression_featureplot_1 <- renderPlot(FeaturePlot(dat_list[[input$coexpression_subset_featureplots]], features = input$coexpression_gene_id_1))
+
+  output$coexpression_featureplot_2 <- renderPlot(FeaturePlot(dat_list[[input$coexpression_subset_featureplots]], features = input$coexpression_gene_id_2))
+
 }
 
 shinyApp(ui, server)
